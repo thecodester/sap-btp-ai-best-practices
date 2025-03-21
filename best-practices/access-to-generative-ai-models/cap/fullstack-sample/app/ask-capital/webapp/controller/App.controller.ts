fbc7284407d218c6f512489ac69e89b783fff98c
Luís Marques <l.marques@sap.com>
2025-03-21 13:08:04 +0000

    Refactor App.controller.ts to improve code clarity and consistency in method implementations

--- best-practices/access-to-generative-ai-models/cap/fullstack-sample/app/ask-capital/webapp/controller/App.controller.ts
+++ best-practices/access-to-generative-ai-models/cap/fullstack-sample/app/ask-capital/webapp/controller/App.controller.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import Controller from "sap/ui/core/mvc/Controller";
-import AppComponent from "../Component";
+import Input from "sap/m/Input";
 import MessageBox from "sap/m/MessageBox";
-import UI5Element from "sap/ui/core/Element";
+import Controller from "sap/ui/core/mvc/Controller";
 import ODataContextBinding from "sap/ui/model/odata/v4/ODataContextBinding";
-import ODataModel from "sap/ui/model/odata/v4/ODataModel";
+import AppComponent from "../Component";
 
 /**
  * @namespace ui5.typescript.helloworld.controller
@@ -12,26 +11,29 @@ import ODataModel from "sap/ui/model/odata/v4/ODataModel";
 export default class App extends Controller {
 	public onInit(): void {
 		// apply content density mode to root view
-		const view = this.getView();
-		if (view) {
-			view.addStyleClass(
+		const oView = this.getView();
+		if (oView) {
+			oView.addStyleClass(
 				(this.getOwnerComponent() as AppComponent).getContentDensityClass()
 			);
 		}
 	}
 
 	public async onSubmit() {
-		const oModel = this.getView()?.getModel();
-		const oActionODataContextBinding: ODataContextBinding | undefined =
-			oModel?.bindContext("/askCapitalOfCountry(...)");
-		const oInputCountry: UI5Element | undefined =
-			this.getView()?.byId("inputCountry");
+		const oView = this.getView()!;
 
-		oActionODataContextBinding?.setParameter(
+		const oModel = oView.getModel();
+		const oActionODataContextBinding = oModel?.bindContext(
+			"/askCapitalOfCountry(...)"
+		) as ODataContextBinding;
+
+		const oInputCountry = oView.byId("inputCountry") as Input;
+
+		oActionODataContextBinding.setParameter(
 			"country",
-			oInputCountry?.getValue()
+			oInputCountry.getValue()
 		);
-		await oActionODataContextBinding?.execute();
+		await oActionODataContextBinding.execute();
 		const oActionContext = oActionODataContextBinding.getBoundContext();
 
 		MessageBox.show(oActionContext.getObject().value);
