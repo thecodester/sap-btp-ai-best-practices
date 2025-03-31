75ffd690663fc9f5eb72f532d2eef15bb901da48
Luís Marques <l.marques@sap.com>
2025-03-31 17:42:28 +0100

    test

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,19 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/custom-agency.css";
 import "./styles/global.css";
 
-export const metadata = {
-  metadataBase: new URL("https://sap-samples.github.io/sap-btp-ai-best-practices/"),
-  title: "SAP BTP AI Best Practices",
-  description: "Your Ultimate Guide for Building AI Solutions on BTP",
-  openGraph: {
-    images: "/images/icon-square.png"
-  },
-  icons: {
-    icon: "/images/icon-square.png",
-    shortcut: "/images/icon-square.png",
-    apple: "/images/icon-square.png"
-  }
-};
+export async function generateMetadata() {
+  return {
+    metadataBase: new URL(process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sap-samples.github.io/sap-btp-ai-best-practices/"),
+    title: "SAP BTP AI Best Practices",
+    description: "Your Ultimate Guide for Building AI Solutions on BTP",
+    openGraph: {
+      images: ["/images/icon-square.png"]
+    },
+    icons: {
+      icon: [{ url: "/images/icon.png" }, { url: "/images/icon-square.png" }, { url: "/images/icon.svg" }],
+      shortcut: "/images/icon-square.png",
+      apple: "/images/icon-square.png"
+    }
+  };
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,22 @@
 import Navbar from "@/components/navigation/NavBar";
 import PageViewTracker from "@/components/tracking/PageViewTracker";
 
+// export async function generateMetadata() {
+//   return {
+//     metadataBase: new URL(process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sap-samples.github.io/sap-btp-ai-best-practices/"),
+//     title: "SAP BTP AI Best Practices",
+//     description: "Your Ultimate Guide for Building AI Solutions on BTP",
+//     openGraph: {
+//       images: ["/images/icon-square.png"]
+//     },
+//     icons: {
+//       icon: [{ url: "/images/icon.png" }, { url: "/images/icon-square.png" }, { url: "/images/icon.svg" }],
+//       shortcut: "/images/icon-square.png",
+//       apple: "/images/icon-square.png"
+//     }
+//   };
+// }
+
 export default async function RootPage() {
   return (
     <>
