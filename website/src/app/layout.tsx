5313c097a0bc8a74e2012cd82161467889535f08
Luís Marques <l.marques@sap.com>
2025-03-31 17:22:06 +0100

    Add metadata configuration to layout.tsx and remove generateMetadata function from page.tsx

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,6 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/custom-agency.css";
 import "./styles/global.css";
 
+export const metadata = {
+  metadataBase: new URL("https://sap-samples.github.io/sap-btp-ai-best-practices/"),
+  title: "SAP BTP AI Best Practices",
+  description: "Your Ultimate Guide for Building AI Solutions on BTP",
+  openGraph: {
+    images: "/images/icon-square.png"
+  },
+  icons: {
+    icon: "/images/icon-square.png",
+    shortcut: "/images/icon-square.png",
+    apple: "/images/icon-square.png"
+  }
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-bs-theme="light">
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,20 +1,5 @@
 import Navbar from "@/components/navigation/NavBar";
 import PageViewTracker from "@/components/tracking/PageViewTracker";
-export async function generateMetadata() {
-  return {
-    metadataBase: new URL("https://sap-samples.github.io/sap-btp-ai-best-practices/"),
-    title: "SAP BTP AI Best Practices",
-    description: "Your Ultimate Guide for Building AI Solutions on BTP",
-    openGraph: {
-      images: "/images/icon-square.png"
-    },
-    icons: {
-      icon: "/images/icon-square.png",
-      shortcut: "/images/icon-square.png",
-      apple: "/images/icon-square.png"
-    }
-  };
-}
 
 export default async function RootPage() {
   return (
