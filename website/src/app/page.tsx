8b18dd4c45c064f8e7b8823e3a7aac23abcf9f53
Luís Marques <l.marques@sap.com>
2025-04-01 11:30:59 +0100

    Add background images and update landing section styles in RootPage component

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,24 @@
 import PageViewTracker from "@/components/tracking/PageViewTracker";
+import { getAssetPath } from "@/utils/paths";
 
 export default async function RootPage() {
   return (
     <>
       <PageViewTracker featureName="HOME" />
-      <section id="landing" className="p-0">
-        <h1>Landing</h1>
+      <section
+        id="landing"
+        className="position-relative w-100"
+        style={{
+          backgroundImage: `url('${getAssetPath("/images/background-left.png")}')`,
+          backgroundSize: "cover",
+          backgroundPosition: "left",
+          backgroundRepeat: "no-repeat",
+          height: "500px"
+        }}
+      >
+        <div className="container h-100 d-flex align-items-center px-4">
+          <h1 className="display-4 fw-bold">Home</h1>
+        </div>
       </section>
     </>
   );
