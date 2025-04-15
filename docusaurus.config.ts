b8e814e7f9d11eee72c8ace95488633328126f0a
Luís Marques <l.marques@sap.com>
2025-04-15 10:09:08 +0100

    Refactor baseUrl assignment in docusaurus.config.ts: simplify configuration by directly using the baseUrl variable for improved clarity and maintainability.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: "https://sap-samples.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.NODE_ENV === "development" ? "/" : "/sap-btp-ai-best-practices/",
+  baseUrl,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
