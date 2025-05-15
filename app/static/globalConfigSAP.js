bc9d43b3a81e6057aab9bf3a48718698ec27abd7
Luís Marques <l.marques@sap.com>
2025-05-15 17:01:37 +0100

    Add global configuration for SAP in globalConfigSAP.js, including trustArc settings and production flag.

---
+++ app/static/globalConfigSAP.js
@@ -0,0 +1,9 @@
+window.SAP = window.SAP || {};
+window.SAP.global = {
+  trustArc: {
+    domain: "www.sap.com",
+    privacyPolicyLink: "https://sap-samples.github.io/sap-btp-ai-best-practices/privacy",
+    cookieLink: "https://sap-samples.github.io/sap-btp-ai-best-practices/cookies"
+  },
+  isProd: false
+};
