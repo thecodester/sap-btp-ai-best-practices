55efeab5e637351e4d75d29e9d2aa6d1e3eb8b39
Luís Marques <l.marques@sap.com>
2025-05-15 22:20:28 +0100

    Deploy website - based on a340c9d3c9b963bc6976f43361a5f41fdd1c24d2

--- globalConfigSAP.js
+++ globalConfigSAP.js
@@ -8,3 +8,12 @@ window.SAP.global = {
   },
   isProd: false // This will be set in the src/client/delayedScripts.ts file using environment variables
 };
+
+// // Create and append div with id "teconsent" to received the link open the cookie consent popup
+// function createCookieConsentDiv() {
+//   const teconsentDiv = document.createElement("div");
+//   teconsentDiv.id = "teconsent";
+//   document.querySelector("body").appendChild(teconsentDiv);
+// }
+
+// createCookieConsentDiv();
