81a2a0b763ec2ba4f57d1e9cd3c832d5fa5fafba
Luís Marques <l.marques@sap.com>
2025-05-15 22:36:54 +0100

    Deploy website - based on a340c9d3c9b963bc6976f43361a5f41fdd1c24d2

--- globalConfigSAP.js
+++ globalConfigSAP.js
@@ -3,8 +3,8 @@ window.SAP = window.SAP || {};
 window.SAP.global = {
   trustArc: {
     domain: "www.sap.com",
-    privacyPolicyLink: "https://sap-samples.github.io/sap-btp-ai-best-practices/privacy",
-    cookieLink: "https://sap-samples.github.io/sap-btp-ai-best-practices/cookies"
+    privacyPolicyLink: "https://btp-ai-bp.docs.sap/privacy",
+    cookieLink: "https://btp-ai-bp.docs.sap/cookies"
   },
   isProd: false // This will be set in the src/client/delayedScripts.ts file using environment variables
 };
